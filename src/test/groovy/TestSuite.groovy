ffaf1ba3ed0816a8ccdec14e9619780f165ee65b
qualityshepherd <brian@qualityshepherd.fake>
2014-04-26 11:47:12 -0500

    Created test suite to run all tests...

---
+++ src/test/groovy/TestSuite.groovy
@@ -0,0 +1,16 @@
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+/**
+ * Created by qualityshepherd on 4/26/14.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses([
+    CLSearchSpec.class,
+    MoodleSandboxLoginSpec.class,
+    WikipediaRecentChangesSpec.class,
+])
+
+public class TestSuite {
+}
